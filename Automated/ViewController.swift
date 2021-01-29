3c1027fc78ecb5c57c57d9bca3c78bcb62a94f59
Karim Ebrahem <karimabdelazizmansour@gmail.com>
2021-01-29 13:41:17 +0200

    Initial Commit

---
+++ Automated/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Automated
+//
+//  Created by Karim Ebrahem on 29/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
